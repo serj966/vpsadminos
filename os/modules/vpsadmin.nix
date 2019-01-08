433e33b88c24744655ecb4957b0f710c803c086d
Jakub Skokan <jakub.skokan@havefun.cz>
2019-01-08 12:45:32 +0100

    os: vpsadmin: start vpsadmin-node

--- os/modules/vpsadmin.nix
+++ os/modules/vpsadmin.nix
@@ -7,6 +7,51 @@ let
 
   cfg = config.vpsadmin;
 
+  vmArgs = pkgs.writeText "vm.args" ''
+    -name vpsadmin@${config.networking.hostName}
+    -setcookie ${cfg.cookie}
+  '';
+
+  appConfig = pkgs.writeText "config.exs" ''
+    use Mix.Config
+
+    config :vpsadmin_queue, :queues, [
+      {:default, 4}
+    ]
+
+    config :vpsadmin_transactional, :supervisor_node, :"${cfg.supervisorNode}"
+  '';
+
+  coreNode = pkgs.vpsadmin.core.node {
+    releaseConfig = pkgs.writeText "rel-config.exs" ''
+      environment :node do
+        set include_erts: true
+        set include_src: false
+        set vm_args: "${vmArgs}"
+        set config_providers: [
+          {Mix.Releases.Config.Providers.Elixir, ["''${RELEASE_ROOT_DIR}/etc/config.exs"]}
+        ]
+        set overlays: [
+          {:copy, "${appConfig}", "etc/config.exs"}
+        ]
+      end
+
+      release :node do
+        set version: current_version(:vpsadmin_base)
+        set applications: [
+          :runtime_tools,
+          :vpsadmin_base,
+          :vpsadmin_queue,
+          :vpsadmin_transactional,
+          :vpsadmin_worker,
+          :vpsadmin_node
+        ]
+      end
+    '';
+    releaseName = "node";
+    releaseEnv = "node";
+  };
+
 in
 
 {
@@ -75,6 +120,21 @@ in
         type = types.str;
         description = "Address for console server to listen on";
       };
+
+      cookie = mkOption {
+        type = types.str;
+        description = ''
+          Cookie for Erlang VM. Has to be set to the same value on all nodes
+          within the cluster.
+        '';
+      };
+
+      supervisorNode = mkOption {
+        type = types.str;
+        description = ''
+          Long name of the supervisor node.
+        '';
+      };
     };
   };
 
@@ -106,6 +166,70 @@ in
       '';
 
       environment.systemPackages = [ pkgs.nodectl ];
+
+      # environment.etc."core-node".source = pkgs.vpsadmin.core.node {};
+      environment.etc."core-node".source = coreNode;
+
+      runit.services.vpsadmin-node = {
+        run = ''
+          user=node
+          dataDir=/var/vpsadmin/core-node
+          overlay=$dataDir/.overlay
+          release=$dataDir/release
+          cgroup=/sys/fs/cgroup/systemd/vpsadmin/node
+
+          export HOME=$dataDir
+          export LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive"
+          export LANG=${config.i18n.defaultLocale}
+
+          chmod og+rx /var/vpsadmin
+
+          if [ -d $cgroup ] ; then
+            kill -SIGTERM `cat $cgroup/cgroup.procs`
+            sleep 5
+
+            if [ `cat $cgroup/cgroup.procs | wc -l` -gt 0 ] ; then
+              kill -SIGKILL `cat $cgroup/cgroup.procs`
+            fi
+
+            sleep 3
+            rmdir $cgroup || exit 1
+          fi
+
+          mkdir -p $cgroup
+          echo $$ > $cgroup/cgroup.procs
+
+          for dir in $overlay $release ; do
+            if [ -d "$dir" ] ; then
+              mountpoint -q $dir && umount -f $dir
+              mountpoint -q $dir && exit 1
+
+              rm -rf $dir
+            fi
+          done
+
+          mkdir -p $overlay $release
+          mount -t tmpfs -osize=256M tmpfs $overlay
+          mkdir -p $overlay/rw $overlay/work
+          mount \
+            -t overlay \
+            -olowerdir=${coreNode},upperdir=$overlay/rw,workdir=$overlay/work \
+            overlay $release
+          mkdir $release/var
+          chown $user $release/var
+
+          exec chpst -u $user:nogroup $release/bin/node foreground
+        '';
+
+        log.enable = true;
+        log.sendTo = "127.0.0.1";
+      };
+
+      users.users.node = {
+        isSystemUser = true;
+        createHome = true;
+        home = "/var/vpsadmin/core-node";
+      };
     })
   ];
 }
--- os/overlays/vpsadmin.nix
+++ os/overlays/vpsadmin.nix
@@ -3,4 +3,6 @@ vpsadmin: self: super:
   libnodectld = super.callPackage "${vpsadmin}/packages/libnodectld" {};
   nodectld = super.callPackage "${vpsadmin}/packages/nodectld" {};
   nodectl = super.callPackage "${vpsadmin}/packages/nodectl" {};
+  vpsadmin.core.node = { ... }@args:
+    super.beam.packages.erlangR21.callPackage "${vpsadmin}/packages/core/generic.nix" args;
 }
